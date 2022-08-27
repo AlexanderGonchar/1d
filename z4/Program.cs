4c615119ee741153fa5b55db43add6b24ec209a8
AlexanderGonchar <goncharas24@gmail.com>
2022-08-27 03:15:58 +0300

    add z4

---
+++ z4/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 4
+
+Console.Write("Введите первое число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+    Console.WriteLine("нет");
+
