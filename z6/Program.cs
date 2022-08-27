32c6a8eee69954e0954be09c51bde6fb3a296a07
AlexanderGonchar <goncharas24@gmail.com>
2022-08-27 03:18:49 +0300

    add z6

---
+++ z6/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.Write("Введите трех значное число : ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Вторая (с начала) цифра : {0}", number.ToString().ToCharArray()[1]);
