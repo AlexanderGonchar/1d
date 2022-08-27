619239b04196340dafddf2c899e46efe9c9be772
AlexanderGonchar <goncharas24@gmail.com>
2022-08-27 03:40:45 +0300

    add z10

---
+++ z10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 100)
+{
+    Console.WriteLine("В числе нет третьей цифры");
+}
+else
+{
+    Console.Write("Третья цифра в числе: ");
+    Console.WriteLine(n % 10);
+}
