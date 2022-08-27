a0e9c84b6a2a4dc65b1346a10e04ec65b682d2b7
AlexanderGonchar <goncharas24@gmail.com>
2022-08-27 03:09:52 +0300

    add z1

---
+++ z1/Program.cs
@@ -0,0 +1,15 @@
+﻿// задача 1
+
+Console.Write("Введите первое число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b == a * a)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
