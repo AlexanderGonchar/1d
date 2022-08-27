28595d6913cddcd9e922168be69ce4575f2faec2
AlexanderGonchar <goncharas24@gmail.com>
2022-08-27 03:17:19 +0300

    add z5

---
+++ z5/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 5
+
+Console.Write("Введите 1 число : ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+int m = int.Parse(Console.ReadLine());
+for (int i = n; i <= m; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
+Console.ReadKey();
+
+
