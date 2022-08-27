426d3bae5eb3b70854318c02b6f542e1ace0d8b5
AlexanderGonchar <goncharas24@gmail.com>
2022-08-27 03:20:27 +0300

    add z7

---
+++ z7/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите чило от 10 до 99:");
+string str = Console.ReadLine();
+
+int max = int.Parse(str[0].ToString());
+
+for (int i = 1; i < str.Length; i++)
+{
+    if (max < int.Parse(str[i].ToString()))
+        max = int.Parse(str[i].ToString());
+}
+Console.WriteLine("Наибольшая цифра = " + max);
+Console.ReadKey();
+
