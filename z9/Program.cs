96a8e2518ca5793e96c58dd783187a9238d4706f
AlexanderGonchar <goncharas24@gmail.com>
2022-08-27 03:29:41 +0300

    add z9

---
+++ z9/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result = a / b;
+if (a % b == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    System.Console.WriteLine("Остаток = " + a % b);
+
+}
+
